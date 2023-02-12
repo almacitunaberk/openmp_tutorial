fb4392def9b8a2b12fdcdc276de5b4b8fa65028a
Tunaberk Almaci <almacitunaberk@gmail.com>
2023-02-12 18:57:33 +0300

    Calculating pi value is changed with Critical section for scalability

--- parallel_integral.c
+++ parallel_integral.c
@@ -2,31 +2,30 @@
 #include <stdio.h>
 
 static long num_steps = 100000;
+#define NUM_THREADS 2
 double step;
-
-#define  NUM_THREADS 2
-
-voind main()
-{
-    int i, nthreads;
-    double pi, sum[NUM_THREADS];
-    step = 1.0/(double) num_steps;
-    omp_set_num_threads(NUM_THREADS);
-    #pragma omp parallel
-    {
-        int i, id, nthrds;
-        double x;
-        id = omp_get_thread_num();
-        nthrds = omp_set_num_threads();
-        if (id == 0) {
-            nthreads = nthrds;
+int main() {
+        double pi = 0.0;
+        step = 1.0 / (double) num_steps;
+        omp_set_num_threads(NUM_THREADS);
+        int  nthreads;
+        #pragma omp parallel
+        {
+                int i, id, nthrds;
+                double x, sum;
+                id  = omp_get_thread_num();
+                nthrds = omp_get_num_threads();
+                if (id == 0)
+                {
+                        nthreads = nthrds;
+                }
+                for(i=id;i<num_steps;i=i+nthrds)
+                {
+                        x = (i + 0.5)*step;
+                        sum += 4.0 / (1.0 + x*x);
+                }
+                #pragma omp critical
+                pi += sum * step;
         }
-        for(i=id, sum[id]=0.0; i<num_steps; i=i+nthrds) {
-            x = (i+0.5)*step;
-            sum[id] += 4.0/(1.0+x*x);
-        }
-    }
-    for(i=0, pi=0.0; i<nthreads; i++) {
-        pi += sum[i] * step;
-    }
+        printf("PI is (%f) \n", pi);
 }
